d5782aea43027f368ddc481038170a677d009008
milesg30 <milesg30@nycstudents.net>
2025-05-15 08:00:24 -0400

    Update SignedText.java

--- Q2/SignedText.java
+++ Q2/SignedText.java
@@ -1,1 +1,31 @@
+public class SignedText
+{
+    private String first, last;
 
+    public SignedText(String first, String last)
+    {
+        this.first = first;
+        this.last = last;
+    }
+
+    public String getSignature()
+    {
+        if(first.length() > 0)
+            return first.substring(0, 1) + "-" + last;
+        else
+            return last;
+    }
+
+    public String addSignature(String message)
+    {
+        String signature = getSignature();
+        int index = message.indexOf(signature);
+
+        if(index == 0)
+            return message.substring(signature.length()) + signature;
+        else if(index == -1)
+            return message + signature;
+        else
+            return message;
+    }
+}
