ad8a0787101c909b264a66cbfca8089cf3cdc3af
milesg30 <milesg30@nycstudents.net>
2025-05-15 08:08:57 -0400

    Update partA.java

--- Q4/partA.java
+++ Q4/partA.java
@@ -1,1 +1,8 @@
+public SumOrSameGame(int numRows, int numCols)
+{
+    puzzle = new int[numRows][numCols];
 
+    for(int r = 0; r < puzzle.length; r++)
+        for(int c = 0; c < puzzle[0].length; c++)
+            puzzle[r][c] = (int) (Math.random() * 9) + 1;
+}
