0efcfef78a25d21a0ed3cfdc7da7c780479f7785
milesg30 <milesg30@nycstudents.net>
2025-05-15 08:03:27 -0400

    Update partA.java

--- Q3/partA.java
+++ Q3/partA.java
@@ -1,1 +1,7 @@
+public Round(String[] names)
+{
+    competitorList = new ArrayList<Competitor>();
 
+    for(int i = 0; i < names.length; i++)
+        competitorList.add(new Competitor(names[i], i + 1));
+}
