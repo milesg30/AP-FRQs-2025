a22d46b4a0b6e8e09d671a38cef9ca945957eab9
milesg30 <milesg30@nycstudents.net>
2025-05-15 08:06:28 -0400

    Update partB.java

--- Q3/partB.java
+++ Q3/partB.java
@@ -1,1 +1,18 @@
+public ArrayList<Match> buildMatches()
+{
+    ArrayList<Match> matches = new ArrayList<Match>();
 
+    int bestIndex = competitorList.size() % 2;
+    int worstIndex = competitorList.size() - 1;
+
+    while(bestIndex < worstIndex)
+    {
+        matches.add(new Match(
+                competitorList.get(bestIndex),
+                competitorList.get(worstIndex)));
+
+        bestIndex++;
+        worstIndex--;
+    }
+
+    return matches;
