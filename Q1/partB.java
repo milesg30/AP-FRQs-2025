68898780fe47ed1050f27640301fb3fc8ada1821
milesg30 <milesg30@nycstudents.net>
2025-05-15 07:56:08 -0400

    Update partB.java

--- Q1/partB.java
+++ Q1/partB.java
@@ -1,1 +1,15 @@
+public int dogWalkShift(int startHour, int endHour)
+{
+    int earnings = 0;
 
+    for(int hour = startHour; hour <= endHour; hour++)
+    {
+        int dogsWalked = walkDogs(hour);
+        earnings += dogsWalked * 5;
+
+        if((hour >= 9 && hour <= 17) || dogsWalked == maxDogs)
+            earnings += 3;
+    }
+
+    return earnings;
+}
