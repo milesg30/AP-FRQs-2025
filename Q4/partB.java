1f57952481afc235846a47985b41f13b71a7b5d1
milesg30 <milesg30@nycstudents.net>
2025-05-15 08:12:57 -0400

    Update partB.java

--- Q4/partB.java
+++ Q4/partB.java
@@ -1,1 +1,21 @@
+public boolean clearPair(int row, int col)
+{
+    for(int r = row; r < puzzle.length; r++)
+    {
+        for(int c = 0; c < puzzle[0].length; c++)
+        {
+            if(r != row || c != col)
+            {
+                if(puzzle[row][col] == puzzle[r][c] ||
+                        puzzle[row][col] + puzzle[r][c] == 10)
+                {
+                    puzzle[row][col] = 0;
+                    puzzle[r][c] = 0;
+                    return true;
+                }
+            }
+        }
+    }
 
+    return false;
+}
